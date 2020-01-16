ea847baf56fa2439a5bb28aa74e4bb8665ed7e62
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2020-01-16 22:35:28 +0100

    Remove duplicate powershell script

--- popup.ps1
+++
@@ -1,9 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-$global:balmsg = New-Object System.Windows.Forms.NotifyIcon
-$path = (Get-Process -id $pid).Path
-$balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
-$balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Information
-$balmsg.BalloonTipText = "Commit has been pushed to remote repo"
-$balmsg.BalloonTipTitle = "Attention $Env:USERNAME"
-$balmsg.Visible = $true
-$balmsg.ShowBalloonTip(1000)
