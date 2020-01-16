93d9b0a556344fc3e5cc87cb9c71030110f2d27b
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2020-01-16 21:43:08 +0100

    Remove extra code in shell script

---
+++ git-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git add -A
+
+NOW=$(date +"%d-%m-%Y")
+
+git commit -m "Updated on $NOW"
+
+"C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe" -File "./msg-popup.ps1"
+
+git push
