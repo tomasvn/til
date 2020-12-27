0f8a6643d9fdce46f74267bbf413e61f427af71d
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2020-12-27 20:20:38 +0100

    update(msg-popup.ps1): Change tooltip text

--- msg-popup.ps1
+++ msg-popup.ps1
@@ -8,7 +8,7 @@ $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
 $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
-$balmsg.BalloonTipText = "Commit has been pushed to remote repo"
+$balmsg.BalloonTipText = "Pushing to remote repo..."
 $balmsg.BalloonTipTitle = "Attention $Env:USERNAME"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(1000)
